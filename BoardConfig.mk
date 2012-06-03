04587be09f47b422386ef3560b047cbda45cc270
Arjen Visser <arjen.jessica@gmail.com>
2012-06-03 19:18:11 +0200

    update BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,7 +10,7 @@ TARGET_KERNEL_CONFIG := cyanogenmod_p505_defconfig
 TARGET_BOOTLOADER_BOARD_NAME := p505
 TARGET_OTA_ASSERT_DEVICE := thunderg,p505
 
-BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p505 no_console_suspend
+BOARD_KERNEL_CMDLINE := mem=88M console=ttyMSM2,115200n8 androidboot.hardware=p505 no_console_suspend
 
 USE_CAMERA_STUB := false
 
