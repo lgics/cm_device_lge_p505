bde4ccdafabc343a5a8d37a764f955c45d71bac4
Arjen Visser <arjen.jessica@gmail.com>
2012-07-08 22:51:52 +0300

    frameworks/base/services/sensorservice/Android.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,16 +1,10 @@
 -include vendor/lge/p505/BoardConfigVendor.mk
 -include device/lge/msm7x27-common/BoardConfigCommon.mk
 
-
-# Nedeed for sensors
-COMMON_GLOBAL_CFLAGS += -DUSE_LGE_ALS_DUMMY
-
 TARGET_KERNEL_CONFIG := cyanogenmod_p505_defconfig
+BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p505 no_console_suspend
 
 TARGET_BOOTLOADER_BOARD_NAME := p505
 TARGET_OTA_ASSERT_DEVICE := thunderg,p505
 
-BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p505 no_console_suspend
-
 USE_CAMERA_STUB := false
-
